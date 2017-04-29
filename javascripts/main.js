89f46c5dba9749d903b95978c086dac14e7f6751
bryanhmiller <bryankalos@hotmail.com>
2017-04-29 10:59:03 -0500

    Added toggle with hide class

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log("I'm totally working.");
+$(document).ready(() => {
+
+	$("#new-item").click(()=>{
+		$(".list-container").addClass("hide");
+		$(".new-container").removeClass("hide");
+	});
+
+	$("#list-items").click(()=>{
+		$(".new-container").addClass("hide");
+		$(".list-container").removeClass("hide");
+	});
+
+
+});
